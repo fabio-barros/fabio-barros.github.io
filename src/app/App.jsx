b0ff7efcf30eb408b76014b284f75b2fade2ebc8
Fábio Barros Ribeiro <whatupfabio@gmail.com>
2021-03-04 06:30:05 -0300

    gh pages portfolio deploy

---
+++ src/app/App.jsx
@@ -0,0 +1,9 @@
+import "../styles/css/App.css";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+function App() {
+    return (
+        <Router>gera</Router>
+    );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/css/Home.css"
+
+const Home = () => {
+    return <div>
+
+    </div>;
+};
+
+export default Home;
